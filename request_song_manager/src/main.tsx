0de0c71b5024a0400a1a301f111b069164760b97
Davaid.Ju <jupioneer8@gmail.com>
2026-06-13 10:01:22 +0800

    新增字体加载功能

--- request_song_manager/src/main.tsx
+++ request_song_manager/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "@/assets/font/飞波正点体.ttf";
+import App from "./App.tsx";
+import loadFont from "./utils/loadFont.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+loadFont().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
---
+++ request_song_manager/src/utils/loadFont.ts
@@ -0,0 +1,20 @@
+/**
+ * 加载飞波正点体字体
+ */
+const loadFont = async () => {
+    const font = new FontFace(
+        'HuaWenFont', // 字体名称，后续统一用这个
+        // 路径 + 修正 format：ttf 对应 truetype
+        `url(${new URL('../assets/font/华文琥珀.ttf', import.meta.url)}) format('truetype')`
+    );
+
+    try {
+        await font.load();
+        document.fonts.add(font);
+        console.log('✅ 华文琥珀 加载成功');
+    } catch (err) {
+        console.error('❌ 华文琥珀加载失败：', err);
+    }
+};
+
+export default loadFont;
